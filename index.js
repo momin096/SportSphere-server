2ada055948dc1f23ca500635dea76ab089f5941d
momin096 <momin09609@gmail.com>
2025-03-08 12:30:12 +0600

    create product post route created

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tp3bo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('coffee server running')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+
+})
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+
+        const productCollection = client.db('sport-sphere').collection('products');
+
+        // create a product
+        app.post('/all-products', async(req,res)=>{
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result)
+        })
+
+        // get products
+        app.get('/all-products', async(req,res)=>{
+            const cursor = productCollection.find().limit(6);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+        // get a product 
+        app.get('/all-products/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await  productCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
